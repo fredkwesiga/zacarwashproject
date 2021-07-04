e8d9d0684bc44d47273ab128d48372aa816d96de
fred kwesiga <fredkwesiga9@gmail.com>
2021-07-04 13:49:29 +0300

    Adds login.pug, layout.pug and images folder

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 //Dependecies
 const express = require('express');
+const path = require('path')
 //instantitions
 const app = express();
 
 //configurations or settings
-app.set('view engine', 'pug');
+// Express.js application to look in the views folder for template files and to use Pug as the template engine.
 app.set('views', './views');
+app.set('view engine', 'pug');
 
 //Middlware
 //adding body parser middleware that handles reading data from the <form> element.
 app.use(express.urlencoded({extended: true}))
-app
+app.use(express.static(path.join(__dirname, "public")));
+
 
 //routes
+//routing the register page
 app.get('/register',(req,res)=>{
   res.render('register', {title:"ZaCar Washer Registry"})
 })
@@ -21,7 +25,14 @@ app.post('/register', (req,res)=>{
   console.log(req.body)
   res.send("The data has been submitted")
 })
-
+//routing the login page
+app.get('/',(req,res)=>{
+  res.render('login', {title:"Login page"})
+})
+app.post('/login', (req,res)=>{
+  console.log(req.body)
+  res.send("Data has been submitted")
+})
 
 
 //handle non exisiting routes
