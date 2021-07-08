48b6b297704ce0fa99386f4986aa2206fc268745
fred kwesiga <fredkwesiga9@gmail.com>
2021-07-08 17:05:46 +0300

    Includes footer in the layout and also puts copy right at the bottom

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependecies
 const express = require('express');
 const path = require('path')
+
 const loginRoutes = require('./routes/loginRoutes')
 const registerRoutes = require('./routes/registerRoutes');
 const carwashtrackingRoutes = require('./routes/carwashtrackingRoutes')
