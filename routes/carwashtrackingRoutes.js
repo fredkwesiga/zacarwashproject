c5dff355c290b424d76b4634878833fa154dfc13
fred kwesiga <fredkwesiga9@gmail.com>
2021-07-12 17:30:42 +0300

    Adds auto update of washers, README.md and redirect function

--- routes/carwashtrackingRoutes.js
+++ routes/carwashtrackingRoutes.js
@@ -2,17 +2,35 @@
 const express = require('express');
 const router = express.Router();
 const Carwashtrack = require('../models/Carwashtrack')
+const Register = require('../models/Register')
 
 
-router.get('/',(req,res)=>{
-  res.render('carwashtracking', {title:"CarWash Tracking System"})
+router.get('/', async(req,res)=>{
+   let washers = await Register.find(); //mongodb function finds members/objects in register collection
+  res.render('carwashtracking', {title:"CarWash Tracking System", washers: washers})
 })
-router.post('/', (req,res)=>{
-  console.log(req.body)
-  const carwashtrack = new Carwashtrack(req.body)
-  carwashtrack.save()
-      .then(()=>{res.send('Car monitor tracks submitted')})
-      .catch((err)=>{console.log(err);res.send('No tracking has been submiited')})
+
+// router.post('/', async(req,res)=>{
+//   console.log(req.body)
+//   const carwashtrack = new Carwashtrack(req.body)
+//   carwashtrack.save()
+//       .then(()=>{res.send('Car monitor tracks submitted')})
+//       .catch((err)=>{console.log(err);res.send('No tracking has been submiited')})
+// })
+
+router.post("/", async(req, res) => {
+    try {
+        const carwashmonitor = new Carwashtrack(req.body);
+        await carwashmonitor.save()
+        res.redirect('carwashtracking?alert=success')
+    }
+    catch (err) {
+        res.status(400).render('carwashtracking', { title: "CarWash Tracking System", alert: 'error' })
+        console.log(err)
+    }
 })
+
 
 module.exports = router;
+
+
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,15 +3,28 @@ const router = express.Router();
 const Register = require('../models/Register')
 
 router.get('/',(req,res)=>{
-  res.render('register', {title:"ZaCar Washer Registry"})
+  res.render('register', {title:"ZaCar Washer Registry", alert: req.query.alert})
 })
 
-router.post('/', (req,res)=>{
-  console.log(req.body)
-  const register = new Register(req.body)
-  register.save()
-    .then(()=>{res.send("The data has been submitted")})
-    .catch((err)=>{console.log(err);res.send("Data not submitted")})
+// router.post('/', (req,res)=>{
+//   console.log(req.body)
+//   const register = new Register(req.body)
+//   register.save()
+//     .then(()=>{res.send("The data has been submitted")})
+//     .catch((err)=>{console.log(err);res.send("Data not submitted")})
+// })
+router.post("/", async(req, res) => {
+    try {
+        const register = new Register(req.body);
+        console.log(register)
+        await register.save()
+        res.redirect('register?alert=success')
+    }
+    catch (err) {
+        res.status(400).render('register', { title: "register", alert: 'error' })
+        console.log(err)
+    }
 })
+
 
 module.exports = router;
